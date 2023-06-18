431534d6a5dfc3b1da4d69145f64046dfc7ce6b2
StMacoli <st.macoli@gmail.com>
2023-06-18 20:25:38 -0300

    first-commit

--- script.js
+++ script.js
@@ -1,46 +1,14 @@
-function carregar() {
-  var msg = window.document.querySelector('#msg');
+function alistador() {
+  let valor = document.getElementById('valorAnalisado');
+  let v = Number(valor.value);
+  let listaValores = document.getElementById('selnum');
+  let arr = [];
 
-  var img = window.document.querySelector('#imagem');
-
-  var title = window.document.querySelector('#header-id');
-  
-  var rodapeNome = window.document.querySelector('#rodape');
-  
-  var data = new Date();
-  var mes = data.getMonth();
-  var dia = data.getDate();
-  var hora = data.getHours();
-  var minutos = data.getMinutes();
+  arr.push(v);
 
 
 
 
+  console.log(v, arr)
 
-  msg.innerHTML = `${dia}/${mes} - ${hora}:${minutos}`;
-
-  if (hora >= 0 && hora < 12) {
-    img.src= 'manha.png'
-    document.body.style.background = "#E0FFFF"
-    document.body.style.color = "#FF8C00"
-    title.style.color = "#FF8C00"
-    rodapeNome.style.color = "#FF8C00"
-  
-  } else if (hora >= 12 && hora < 18) {
-    img.src = 'tarde.png'
-    document.body.style.background = "#FFDAB9"
-    document.body.style.color = "#CD853F"
-    title.style.color = '#CD853F'
-    rodapeNome.style.color = '#CD853F'
-
-    
-  } else {
-    img.src = 'noite.png'
-    document.body.style.background = "#483D8B"
-    document.body.style.color = "#CD853F"
-    title.style.color = '#CD853F'
-    rodapeNome.style.color = '#CD853F'
-
-    
-  }
 }
